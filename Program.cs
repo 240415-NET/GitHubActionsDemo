3dc897cd348e3380d24aff6c7146286d203ae11d
jdelacruz96 <jonathan.delacruz@revature.com>
2024-07-03 09:28:07 -0400

    changed program.cs

--- Program.cs
+++ Program.cs
@@ -10,12 +10,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // To serve Swagger UI at the app's root (http://localhost:<port>/)
+    });
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
